53484ea06f9b7784c01cc39f78d7b047fdb4b916
veartdesigns <veartpc@gmail.com>
2018-12-06 03:15:53 +0100

    UI graphics added

---
+++ Assets/VeartDesigns/Scripts/Language/LocalizeString.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizeString : MonoBehaviour {
+
+    public Text Text;
+    public LanguageTag LanguageTag;
+	// Use this for initialization
+	void Start () {
+
+        Text.text = MainController.Instance.GetText(LanguageTag);
+	}
+}
---
+++ Assets/VeartDesigns/Scripts/UserActionController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UserActionController : MonoBehaviour
+{
+    public EventSystem EventSystem;
+    private float _secondsToBackToInit = 25f;
+    private float _startTime;
+
+    private void Awake(){
+
+        _startTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (EventSystem.current.IsPointerOverGameObject()){
+                RestartCounterTime();
+            }
+        }
+            CheckTime();
+    }
+
+    private void RestartCounterTime(){
+
+        _startTime = Time.time;
+    }
+
+    private void CheckTime()
+    {
+        float currentTime = Time.time;
+        float timePassed = currentTime - _startTime;
+        if (timePassed >= _secondsToBackToInit)
+        {
+            _startTime = Time.time;
+            Debug.Log("NO ACTION, BACK TO INIT MENU");
+            MainController.Instance.BackToInitScene();
+        }
+    }
+}
